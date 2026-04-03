63b01998ae16631b4be0ab683ec1f4afa629e742
callmechung <trungtxtx28032005@gmail.com>
2026-04-03 08:07:47 +0700

    add create task mqtt

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "global.h"
 
 #include "task_WIFI.h"
+#include "task_mqtt.h"
 
 #include "task_dht.h"
 #include "task_light.h"
@@ -13,8 +14,9 @@ void setup()
   xSensor = xSemaphoreCreateMutex();
 
   Serial.println("Set up begin");
-  
+
   xTaskCreate(task_wifi, "TASK WIFI", 4096, NULL, 2, NULL);
+  xTaskCreate(task_mqtt, "TASK MQTT", 4096, NULL, 2, NULL);
   xTaskCreate(task_dht, "TASK DHT", 2048, NULL, 1, NULL);
   xTaskCreate(task_ultrasonic, "TASK ULTRASONIC", 2048, NULL, 1, NULL);
   xTaskCreate(task_soil, "TASK_SOIL", 2048, NULL, 1, NULL);
